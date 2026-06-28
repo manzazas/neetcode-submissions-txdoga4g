9dea34fa0b1cc42f88d3f152f18f5fc800ee0848
Zachary Stybel <151088657+manzazas@users.noreply.github.com>
2026-06-28 11:51:55 -0400

    Add: is-palindrome - submission-27

---
+++ Data Structures & Algorithms/is-palindrome/submission-27.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        l = 0
+        r = len(s) - 1
+
+        while l <= r:
+            while s[l].isalnum() == False and l < r:
+                l += 1
+            while s[r].isalnum() == False and l < r:
+                r -= 1
+            if l > r:
+                return False;
+            if s[l].lower() != s[r].lower():
+                return False;
+            else:
+                l += 1
+                r -= 1
+
+        return True;
+        
