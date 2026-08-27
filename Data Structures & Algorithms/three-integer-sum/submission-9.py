ce682ace892135cd559b5febcf05ffe2695e3d13
Zachary Stybel <151088657+manzazas@users.noreply.github.com>
2026-08-27 14:24:13 -0400

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        valids = []
+        nums.sort()
+        if nums[0] > 0:
+            return []
+
+
+        def twosum(target, start):
+            hash = {}
+            for i in range(start,len(nums)):
+                if nums[i] in hash:
+                    tuplit = [-target, hash[nums[i]], nums[i]]
+                    if tuplit not in valids:
+                        valids.append(tuplit)
+        
+                hash[target - nums[i]] = nums[i]
+        for i in range(0,len(nums) - 2):
+            twosum(-nums[i], i + 1)
+
+        return valids
+
+
+        # sorting: nlogn
+        # outer loop: n
+        # inner loop: n
