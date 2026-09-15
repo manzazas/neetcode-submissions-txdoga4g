d7d22f814691e5c56b5fe7a378e394e5d1ce5b22
Zachary Stybel <151088657+manzazas@users.noreply.github.com>
2026-09-15 09:39:50 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stacky = []
+        res = [0] * len(temperatures)
+        for i, temp in enumerate(temperatures):
+            if not stacky:
+                stacky.append([temp,i])
+            else:
+                while stacky and stacky[-1][0] < temp:
+                    prevTempIndex = stacky.pop()
+                    res[prevTempIndex[1]] = i - prevTempIndex[1]
+                stacky.append([temp,i])
+
+
+        return res
+
+        
